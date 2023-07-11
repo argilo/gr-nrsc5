8a68715f7d91eceedb4ff9c0486d8e48170b0738
Clayton Smith <argilo@gmail.com>
2023-07-11 18:59:13 -0400

    Remove extra whitespace

--- python/lot_encoder.py
+++ python/lot_encoder.py
@@ -32,7 +32,7 @@ class lot_encoder(gr.basic_block):
         self.parts = []
         with open(self.filename, "rb") as f:
             data = f.read()
-        
+
         for offset in range(0, len(data), 256):
             chunk = data[offset:offset+256]
             seq = offset // 256
@@ -41,9 +41,9 @@ class lot_encoder(gr.basic_block):
                 header_len = 24 + len(self.filename.encode())
             else:
                 header_len = 8
-            
+
             repeat = 1
-            
+
             header = struct.pack("<BBHI", header_len, repeat, self.lot_id, seq)
             if seq == 0:
                 version = 1
@@ -51,16 +51,16 @@ class lot_encoder(gr.basic_block):
                 size = len(data)
                 mime = 0x4f328ca0  # PNG
                 header += struct.pack("<IIII", version, expiry, size, mime) + self.filename.encode()
-            
+
             self.parts.append(header + chunk)
-        
+
         self.aas_seq = 0
         self.timer = threading.Timer(self.INTERVAL, self.send)
         self.timer.start()
-    
+
     def stop(self):
         self.timer.cancel()
-        
+
     def send(self):
         print(f"Sending LOT file: {self.filename}")
         for part in self.parts:
@@ -68,6 +68,6 @@ class lot_encoder(gr.basic_block):
             msg = pmt.cons(pmt.make_dict(), pmt.init_u8vector(len(aas_pdu), list(aas_pdu)))
             self.message_port_pub(pmt.intern("aas"), msg)
             self.aas_seq = (self.aas_seq + 1) & 0xffff
-        
+
         self.timer = threading.Timer(self.INTERVAL, self.send)
         self.timer.start()
