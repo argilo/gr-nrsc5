655304144a12e4194873d2a583327234320ad9ed
Clayton Smith <argilo@gmail.com>
2024-08-16 13:49:29 -0400

    Add support for SAME, FIPS and ZIP codes

--- apps/send_alert.py
+++ apps/send_alert.py
@@ -3,6 +3,7 @@
 import argparse
 import socket
 import os
+import sys
 
 
 def append_int(command_bits, n, length):
@@ -11,29 +12,49 @@ def append_int(command_bits, n, length):
         n >>= 1
 
 
-def fips_cardinal(fips_code):
-    return (fips_code) // 100000
+def middle_digits(location):
+    return (location % 100000) // 1000
 
 
-def fips_state(fips_code):
-    return (fips_code % 100000) // 1000
-
-
-def fips_county(fips_code):
-    return fips_code % 1000
+def compress(location):
+    return ((location // 100000) * 1000) + (location % 1000)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("category", type=int)
 parser.add_argument("message")
+parser.add_argument("--same", type=int, action="append", default=[])
 parser.add_argument("--fips", type=int, action="append", default=[])
+parser.add_argument("--zip", type=int, action="append", default=[])
 parser.add_argument("--test", action="store_true")
 parser.add_argument("--host", default="localhost")
 parser.add_argument("--port", type=int, default=52000)
 args = parser.parse_args()
 
-fips = sorted(args.fips)
-num_locations = len(fips)
+if bool(args.same) + bool(args.fips) + bool(args.zip) > 1:
+    print("All locations must use the same format", file=sys.stderr)
+    exit(1)
+
+location_format = 0
+locations = args.same
+full_len = 20
+compressed_len = 14
+if args.fips:
+    location_format = 1
+    locations = args.fips
+    full_len = 17
+    compressed_len = 10
+elif args.zip:
+    location_format = 2
+    locations = args.zip
+    full_len = 17
+    compressed_len = 10
+
+if len(locations) > 31:
+    print("Can't use more than 31 locations", file=sys.stderr)
+    exit(1)
+
+locations.sort()
 
 command_bits = []
 append_int(command_bits, 0, 8)                                # unknown
@@ -41,20 +62,22 @@ append_int(command_bits, 0, 12)                               # CRC
 append_int(command_bits, 0x49, 8)                             # unknown
 append_int(command_bits, args.category, 5)                    # category type 1
 append_int(command_bits, 30 if args.test else 0, 5)           # category type 2
-append_int(command_bits, 0b000101101101, 12)                  # unknown
-append_int(command_bits, num_locations, 6)                    # number of locations
+append_int(command_bits, 0b101101101, 9)                      # unknown
+append_int(command_bits, location_format, 3)                  # location format
+append_int(command_bits, len(locations), 5)                   # number of locations
+command_bits.append(0)                                        # unknown
 
-if num_locations >= 1:
-    append_int(command_bits, fips[0], 20)
+if len(locations) >= 1:
+    append_int(command_bits, locations[0], full_len)
 
-for i in range(1, num_locations):
-    if fips_state(fips[i]) == fips_state(fips[i-1]):
+for i in range(1, len(locations)):
+    if middle_digits(locations[i]) == middle_digits(locations[i-1]):
         command_bits.append(0)
-        compressed_fips = fips_cardinal(fips[i]) * 1000 + fips_county(fips[i])
-        append_int(command_bits, compressed_fips, 14)
+        append_int(command_bits, compress(locations[i]), compressed_len)
     else:
         command_bits.append(1)
-        append_int(command_bits, fips[i], 20)
+        append_int(command_bits, locations[i], full_len)
+
 
 while len(command_bits) % 16 != 8:
     command_bits.append(0)
@@ -68,7 +91,7 @@ for start in range(0, len(command_bits), 8):
 command_bytes = bytes(command_bytes)
 
 command = f"set_alert|{command_bytes.hex()}|{args.message}\n"
-print(command)
+print(command[:-1])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((args.host, args.port))
 s.send(command.encode())
